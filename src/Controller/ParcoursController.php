16443d4c81fdf881e53d332508a9a07087c32931
Julia Mitropolskiy <jkovaleva@gmail.com>
2020-09-11 11:48:33 +0200

    creation parcours

--- src/Controller/ParcoursController.php
+++ src/Controller/ParcoursController.php
@@ -5,15 +5,47 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Parcours;
+use App\Repository\ParcoursRepository;
+
+
 class ParcoursController extends AbstractController
 {
     /**
-     * @Route("/parcours", name="parcours")
+     * @Route("/", name="parcours")
      */
-    public function index()
+    public function index(ParcoursRepository $repo)
     {
-        return $this->render('parcours/index.html.twig', [
-            'controller_name' => 'ParcoursController',
+        $parcoursList=$repo->findAll();
+        return $this->render('parcours/parcours.html.twig', [
+            'parcoursList'=>$parcoursList
         ]);
+    }
+
+    /**
+     * @Route("parcours/create", name="app_create_parcours", methods={"get", "post"})
+     */
+    public function createParcours(Request $request, EntityManagerInterface $em) : Response {
+
+        if ($request->isMethod("POST")){
+            $data=$request->request->all();
+            $parcours = new Parcours();
+            $parcours->setName($data['name']);
+            $parcours->setDuration($data['duration']);
+            // $parcours->setRegion($data['region']);
+
+            $em->persist($parcours);
+            $em->flush();
+
+            return $this->redirectToRoute('parcours');
+        }
+
+        return $this->render("parcours/newParcours.html.twig");
+
     }
 }
