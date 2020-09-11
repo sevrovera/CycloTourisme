e3ac037bc6b1af3ce113ae7071194965125cd193
Sev Rovera <sev.rovera@gmail.com>
2020-09-11 10:31:36 +0200

    Controllers pour les Parcours et Regions

---
+++ src/Controller/ParcoursController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ParcoursController extends AbstractController
+{
+    /**
+     * @Route("/parcours", name="parcours")
+     */
+    public function index()
+    {
+        return $this->render('parcours/index.html.twig', [
+            'controller_name' => 'ParcoursController',
+        ]);
+    }
+}
