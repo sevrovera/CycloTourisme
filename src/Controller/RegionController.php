716c7eaed3dce173e233a7fda8893e3153d87c1b
Sev Rovera <sev.rovera@gmail.com>
2020-09-11 11:10:09 +0200

    Liste des régions + Création nouvelle région

--- src/Controller/RegionController.php
+++ src/Controller/RegionController.php
@@ -5,15 +5,46 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Region;
+use App\Repository\RegionRepository;
+
+
 class RegionController extends AbstractController
 {
     /**
-     * @Route("/region", name="region")
+     * @Route("/regions", name="regions")
      */
-    public function index()
+    public function index(RegionRepository $repo)
     {
-        return $this->render('region/index.html.twig', [
-            'controller_name' => 'RegionController',
-        ]);
+        $regions=$repo->findAll();
+        return $this->render('region/index.html.twig', ['regions'=>$regions]);
+
     }
+
+    /**
+     * @Route("/create/region", name="add_region", methods={"get", "post"})
+     */
+    public function createRegion(Request $request, EntityManagerInterface $em) : Response {
+
+        if ($request->isMethod("POST")){
+            $data=$request->request->all();
+            $region = new Region();
+            $region->setName($data['name']);
+
+            // insertion de la nouvelle région en db avec Doctrine
+            $em->persist($region);
+            // équivalent d'un commit (peut se faire après plusieurs actions de persistance)
+            $em->flush();
+
+            return $this->redirectToRoute('regions');
+        }
+
+        return $this->render("region/newRegion.html.twig");
+    }
+
 }
